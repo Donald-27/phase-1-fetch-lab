d0c0da9be95a68b79998d5a4a0d5bf84691d1cb7
Donald-27 <+>
2025-03-29 12:53:42 +0300

    fetched data from anapiapi

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+
+    return fetch("https://anapioficeandfire.com/api/books")
+      .then((resp) => resp.json())  
+      .then((json) => renderBooks(json));
+      rednerBooks(json);  
+  }
+   
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +21,9 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
+fetch("https://anapioficeandfire.com/api/books")
+.then((resp) => resp.json())
+.then((json) => console.log(json))
