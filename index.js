559d187599c076f2a146bc03286618e51afc3998
Donald-27 <+>
2025-03-29 12:58:41 +0300

    modified index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function fetchBooks() {
     return fetch("https://anapioficeandfire.com/api/books")
       .then((resp) => resp.json())  
       .then((json) => renderBooks(json));
-      rednerBooks(json);  
+      renderBooks(json);  
   }
    
 
